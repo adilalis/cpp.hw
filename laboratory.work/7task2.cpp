c9d41086a17a79bf92155d10003652bb9c05e6f5
Adilette <alishev.adilet2008@gmail.com>
2026-03-13 18:26:26 +0500

    Add number guessing game in 7task2.cpp
    This program generates a random number between 10 and 99 and allows the user to guess it, providing feedback on whether the guess is too high or too low. The game continues until the user guesses correctly or enters a negative number to stop.

---
+++ laboratory.work/7task2.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main() {
+    srand(time(0));
+
+    int secret = rand() % 90 + 10; // число от 10 до 99
+    int guess;
+    int attempts = 0;
+
+    cout << "Guess the number (10-99)" << endl;
+
+    while (true) {
+        cin >> guess;
+
+        if (guess < 0) {
+            cout << "Game stopped." << endl;
+            break;
+        }
+
+        attempts++;
+
+        if (guess == secret) {
+            cout << "Correct!" << endl;
+            cout << "Attempts: " << attempts << endl;
+            break;
+        }
+        else if (guess < secret) {
+            cout << "More" << endl;
+        }
+        else {
+            cout << "Less" << endl;
+        }
+    }
+
+    cout << "Secret number was: " << secret << endl;
+
+    return 0;
+}
+Как работает программа
