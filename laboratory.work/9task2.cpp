731f6d9ac58a3720ba6c21d766e97f55a32bcf53
Adilette <alishev.adilet2008@gmail.com>
2026-03-27 14:24:57 +0500

    Add string sorting function using bubble sort
    Implement a bubble sort algorithm to sort a string.

---
+++ laboratory.work/9task2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void sortString(string& s) {
+    int n = s.length();
+    
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (s[j] > s[j + 1]) {
+                char temp = s[j];
+                s[j] = s[j + 1];
+                s[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    string s = "dbca";
+    sortString(s);
+    cout << s; // abcd
+}
