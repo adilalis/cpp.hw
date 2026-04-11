843c90babfe8fb2c251789a4e5994eaad32a40f1
Adilette <alishev.adilet2008@gmail.com>
2026-04-11 14:14:13 +0500

    Refactor sumDepth2 function and update output messages

--- laboratory.work/12task4.cpp
+++ laboratory.work/12task4.cpp
@@ -1,8 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
 int sumDepth2(const vector<int>& h) {
     int s = 0;
     for (int i = 0; i < (int)h.size(); i++) {
-        int depth = (int)(log2(i + 1));
-        if (depth == 2) s += h[i];
+        int depth = (int)log2(i + 1);
+        if (depth == 2) {
+            s += h[i];
+        }
     }
     return s;
 }
@@ -10,11 +18,17 @@ int sumDepth2(const vector<int>& h) {
 int main() {
     vector<int> heap = {42, 17, 35, 8, 23, 30, 15, 5, 11, 19, 7, 28};
 
+    // строим max-heap
     make_heap(heap.begin(), heap.end());
 
-    cout << "Куча: ";
-    for (int x : heap) cout << x << " ";
+    cout << "Куча (массив): ";
+    for (int x : heap)
+        cout << x << " ";
     cout << endl;
 
-    cout << "Сумма глубины 2: " << sumDepth2(heap) << endl;
+    cout << "Корень (максимум): " << heap[0] << endl;
+
+    cout << "Сумма узлов на глубине 2: " << sumDepth2(heap) << endl;
+
+    return 0;
 }
