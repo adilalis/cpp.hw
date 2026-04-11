533af3bb08fb233006632fc004f0b7a645e93943
Adilette <alishev.adilet2008@gmail.com>
2026-04-11 14:12:50 +0500

    Update 12task2.cpp

--- laboratory.work/12task2.cpp
+++ laboratory.work/12task2.cpp
@@ -13,3 +13,22 @@ long long sumColMajor() {
             s += A[i][j];
     return s;
 }
+
+    auto t1 = high_resolution_clock::now(); [cite: 6]
+    auto s1 = sumRowMajor(); [cite: 6]
+    auto t2 = high_resolution_clock::now(); [cite: 6]
+
+    auto t3 = high_resolution_clock::now(); [cite: 6]
+    auto s2 = sumColMajor(); [cite: 6]
+    auto t4 = high_resolution_clock::now(); [cite: 6]
+
+    cout << "Row-major: " 
+         << duration_cast<microseconds>(t2 - t1).count() 
+         << endl; [cite: 6]
+
+    cout << "Col-major: " 
+         << duration_cast<microseconds>(t4 - t3).count() 
+         << endl; [cite: 6]
+
+    return 0;
+}
